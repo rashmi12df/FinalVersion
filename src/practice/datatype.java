944ecb8b6dc9a3dcfb245f85ab6b4acbdcf0e5d4
unknown <rashmisalunke0409@gmail.com>
2022-11-30 09:09:34 +0530

    hereis final

---
+++ src/practice/datatype.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class datatype {
+	public static void main(String[] args) {
+		int a=40;
+		System.out.println(a);
+		
+		
+		
+	
+		
+		
+	}
+
+}
---
+++ src/practice/primeno.java
@@ -0,0 +1,31 @@
+package practice;
+
+public class primeno {
+	public static void main(String[] args) {
+		int count=0;
+		for(int i=2; i<100; i++)
+		{
+			for(int j=2; j<i; j++)
+			{
+				if(i%j==0)
+				{
+					count++;
+				}
+			}
+			if(count==0) {
+				System.out.println(i);
+			}
+			count=0;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
